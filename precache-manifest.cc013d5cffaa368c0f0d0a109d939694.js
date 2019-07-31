029eb146011e116df6ffa4edf8e46377a54759e5
Utkarsh Chauhan <utchauha@microsoft.com>
2019-07-31 23:26:36 +0530

    Updates

---
+++ precache-manifest.cc013d5cffaa368c0f0d0a109d939694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3402ff7cbe3cb1d50f272659cf94415a",
+    "url": "/chauhan-utk/GitHub-Card-App/index.html"
+  },
+  {
+    "revision": "ce49b48862973d1e19d8",
+    "url": "/chauhan-utk/GitHub-Card-App/static/css/main.75a35299.chunk.css"
+  },
+  {
+    "revision": "fb657d40ff019ea2e68d",
+    "url": "/chauhan-utk/GitHub-Card-App/static/js/2.eeba1995.chunk.js"
+  },
+  {
+    "revision": "ce49b48862973d1e19d8",
+    "url": "/chauhan-utk/GitHub-Card-App/static/js/main.086e52a9.chunk.js"
+  },
+  {
+    "revision": "eb11565d923a78347571",
+    "url": "/chauhan-utk/GitHub-Card-App/static/js/runtime~main.2e417749.js"
+  }
+]);
